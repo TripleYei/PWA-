cf756ed829ed8db14112422625e0fa83dee7ac14
TripleYei <58819156+TripleYei@users.noreply.github.com>
2022-10-14 21:15:26 +0200

    Add files via upload

---
+++ code/app-about.cb14bf9a.js
@@ -0,0 +1,24 @@
+import{i as l,s as o,y as c,a as u,e as d}from"./index.e18007de.js";const h=l`
+  @media(min-width: 1000px) {
+    sl-card {
+      max-width: 70vw;
+    }
+  }
+`;var v=Object.defineProperty,f=Object.getOwnPropertyDescriptor,m=(i,r,s,a)=>{for(var e=a>1?void 0:a?f(r,s):r,p=i.length-1,t;p>=0;p--)(t=i[p])&&(e=(a?t(r,s,e):t(e))||e);return a&&e&&v(r,s,e),e};let n=class extends o{constructor(){super()}render(){return c`
+      <app-header ?enableBack="${!0}"></app-header>
+
+      <main>
+        <h2>SOBRE MI</h2>
+
+        <sl-card>
+          <h2>Bienvenido/a</h2>
+
+          <p>
+		Gracias, por visitar nuestra aplicación web que se puede instalar en el teléfono
+          </p>
+
+          
+        </sl-card>
+  </main>
+    `}};n.styles=[u,h];n=m([d("app-about")],n);export{n as AppAbout};
+//# sourceMappingURL=app-about.cb14bf9a.js.map
